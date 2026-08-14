77456b5e22778bc2063a355293d53e75c33d630f
sujith <saisujith.reddy@ahex.co.in>
2026-08-14 23:35:05 +0530

    added packages for swagger documentation

--- src/main/java/com/vamshi/HospitalManagementSystem/auth/security/SecurityConfig.java
+++ src/main/java/com/vamshi/HospitalManagementSystem/auth/security/SecurityConfig.java
@@ -40,6 +40,11 @@ public class SecurityConfig {
                                 .authorizeHttpRequests(auth -> auth
                                                 .requestMatchers("/api/auth/**")
                                                 .permitAll()
+                                                .requestMatchers(
+                                                                "/swagger-ui/**",
+                                                                "/swagger-ui.html",
+                                                                "/v3/api-docs/**")
+                                                .permitAll()
                                                 .requestMatchers("/api/admin/**")
                                                 .hasRole("ADMIN")
                                                 .requestMatchers(HttpMethod.POST,
